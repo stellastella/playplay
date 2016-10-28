64b8472cde8640ecd94c5b8144ab91a093f3b47e
stellastella <wuchukwustella333@gmail.com>
2016-10-28 08:37:40 +0100

    upload changes

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
 
 <form action="imageupload.php" method="post"  enctype="multipart/form-data">
         Select image to upload:
-        <input type="file" name="file" id="file">
+    
         <input type="submit" value="submit" name="submit"> </form>
 
 </body>
